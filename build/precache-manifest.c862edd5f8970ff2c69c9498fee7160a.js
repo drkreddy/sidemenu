1babe3eca56d39db454b71254bb0b1b1826081ee
Donapati Ravindra Kumar Reddy <util4ravi@gmail.com>
2019-02-27 08:57:06 +0530

    added build for testing

---
+++ build/precache-manifest.c862edd5f8970ff2c69c9498fee7160a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2daff4e01734cd428e1",
+    "url": "/static/js/main.c2daff4e.chunk.js"
+  },
+  {
+    "revision": "c1a1bdf3bcd7b2e98ef7",
+    "url": "/static/js/2.c1a1bdf3.chunk.js"
+  },
+  {
+    "revision": "c2daff4e01734cd428e1",
+    "url": "/static/css/main.8e09896a.chunk.css"
+  },
+  {
+    "revision": "4782587e63754dd868aa6246fd464402",
+    "url": "/index.html"
+  }
+];
